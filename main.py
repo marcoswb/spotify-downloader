e67af3e674b35d904929ce23f731e1e4bdc8cfc3
Marcos Warmling Berti <marcos_+wb@outlook.com>
2022-09-13 20:38:55 -0300

    download with spotify_dl

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from sys import exit
+from dotenv import load_dotenv
+from os import getenv, system
 from PySide6.QtWidgets import QApplication, QMainWindow
 from resources.screen import UiMainWindow
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
 from utils.functions import *
 from utils.components import *
@@ -11,6 +15,17 @@ class Main(QMainWindow):
         self.__window = UiMainWindow()
         self.__window.setup_ui(self)
         self.link_components()
+
+        load_dotenv()
+
+        client_id = getenv('SPOTIFY_CLIENT_ID')
+        client_secret = getenv('SPOTIFY_CLIENT_SECRET')
+
+        self.spotify_client = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(
+            client_id=client_id,
+            client_secret=client_secret
+        ))
+
     
 
     def link_components(self):
@@ -23,7 +38,6 @@ class Main(QMainWindow):
         self.__progressbar_status_download = self.__window.progressbar__status_download
 
         self.__button_download.clicked.connect(self.download)
-        pass
     
 
     def init(self):
@@ -37,12 +51,20 @@ class Main(QMainWindow):
 
     def download(self):
         playlist_link = self.__textbox_playlist_link.text()
+        output_folder = '~/Downloads'
 
         if is_null(playlist_link):
             error_message(self, 'Preencha a campo de link da playlist!')
             self.__textbox_playlist_link.clear()
             self.__textbox_playlist_link.setFocus()
-            return        
+            return
+
+        for track in self.spotify_client.playlist_tracks(playlist_link)['items']:
+            url = track['track']['external_urls']['spotify']
+            print(url)
+            system(f"spotify_dl -l '{url}' -o {output_folder}")
+
+        show_message(self, 'Processo Finalizado', 'FIM.')
     
 
 if __name__ == '__main__':
